f26cb7d893d39a96dd95d301eff51c76ed04ce47
dhanjrt26 <danilotorrejosjr@gmail.com>
2025-06-02 02:00:19 +0800

    added about page

---
+++ sanmiguel/src/main/java/controller/CustomErrorController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+ /*
+    @RequestMapping("/error")
+    public String handleError() {
+        /* return "errorx"; // Redirects to error.html 
+    }
+
+     */
+}
