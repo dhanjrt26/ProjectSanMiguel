1c44831106f1a45b86b21f3a7889075978474868
dhanjrt26 <danilotorrejosjr@gmail.com>
2025-06-01 17:13:22 +0800

    first commit

---
+++ sanmiguel/src/main/java/project/sanmiguel/sanmiguel/ProjectSanMiguelApplication.java
@@ -0,0 +1,14 @@
+package project.sanmiguel.sanmiguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSanMiguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSanMiguelApplication.class, args);
+	}
+
+	
+}
---
+++ sanmiguel/src/test/java/project/sanmiguel/sanmiguel/ProjectSanMiguelApplicationTests.java
@@ -0,0 +1,13 @@
+package project.sanmiguel.sanmiguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSanMiguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
